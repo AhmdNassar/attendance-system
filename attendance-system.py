cadf4d272faac079f6a4caf1133d1ce20b4b6f74
Ahmed Nassar <Ahmednassar@gmail.com>
2018-11-01 01:24:02 +0200

    add creatData & appendData & takePic functions

--- attendance-system.py
+++ attendance-system.py
@@ -1,7 +1,48 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 27 14:51:14 2018
+import pandas as pd
+import cv2
+import os
 
-@author: A.Nassar
-"""
+def creatData():
+    if not os.path.exists("attendance-data"):
+        os.makedirs("attendance-data")
+    data = {"ID":[],"Name":[],"Path":[]}
+    if not os.path.isfile("attendance-data\\data.csv"):
+        df = pd.DataFrame(data = data,columns=['ID','Name','Path'])
+        df.to_csv("attendance-data\\data.csv")
 
+
+def appendData(name,path):
+    df = pd.read_csv("attendance-data\\data.csv",index_col=0)
+    newID = len(df["ID"]) + 1
+    data = {'ID':newID,'Name':name,'Path':path}
+    df.loc[newID-1] = data
+    df.to_csv("attendance-data\\data.csv")
+
+def takePic(name,id):
+    path = 'attendance-data\\{} - {}'.format(id,name)
+    if not os.path.exists(path):
+        os.makedirs('attendance-data\\{} - {}'.format(id,name))
+    print("Please take 5 photos!, press Enter to take photo")
+    cam = cv2.VideoCapture(0)
+    key = cv2.waitKey(1)
+    imageCounter = 0
+    while(True):
+        ret,frame = cam.read()
+        cv2.imshow("Take Photos",frame)
+        if not ret :
+            print("error in take photos!")
+            break
+        if imageCounter==5:
+            print("thank you your 5 photos saved!")
+            break
+        elif key%256 == 13:
+            imgName = "{}-pic-{}.png".format(name,imageCounter+1)
+            imaPath = path + imgName
+            cv2.imwrite(imaPath,frame)
+            print("image {} saved".format(imgName))
+            imageCounter+=1
+    cam.release()
+    cv2.destroyAllWindows()
+    return path
+
+takePic("ahmed","1")
