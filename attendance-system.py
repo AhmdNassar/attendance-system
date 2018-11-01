dd2da599a38185de1fece45404e23dac9334b814
Ahmed Nassar <Ahmednassar@gmail.com>
2018-11-01 23:06:42 +0200

    fix cam error

--- attendance-system.py
+++ attendance-system.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import cv2
 import os
+import numpy as np
 
 def creatData():
     if not os.path.exists("attendance-data"):
@@ -24,7 +25,6 @@ def takePic(name,id):
         os.makedirs('attendance-data\\{} - {}'.format(id,name))
     print("Please take 5 photos!, press Enter to take photo")
     cam = cv2.VideoCapture(0)
-    key = cv2.waitKey(1)
     imageCounter = 0
     while(True):
         ret,frame = cam.read()
@@ -32,13 +32,14 @@ def takePic(name,id):
         if not ret :
             print("error in take photos!")
             break
+        key = cv2.waitKey(1)
+
         if imageCounter==5:
             print("thank you your 5 photos saved!")
             break
         elif key%256 == 13:
             imgName = "{}-pic-{}.png".format(name,imageCounter+1)
-            imaPath = path + imgName
-            cv2.imwrite(imaPath,frame)
+            cv2.imwrite(os.path.join(path,imgName),frame)
             print("image {} saved".format(imgName))
             imageCounter+=1
     cam.release()
@@ -46,3 +47,49 @@ def takePic(name,id):
     return path
 
 takePic("ahmed","1")
+
+
+"""cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF!=255:
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+"""
+"""
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("FivePhotos")
+
+img_counter = 1
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow("FivePhotos", frame)
+    if not ret:
+        break
+    k = cv2.waitKey(1)
+
+    if img_counter == 6:
+        print("Thank's for your time.")
+        break
+    elif k%256 == 32: #Space
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        path = 'Your Directory Path'+str(num)
+        #cv2.imwrite(img_name, frame)
+        createfolder(str(num))
+        cv2.imwrite(os.path.join(path , img_name), frame)
+        #print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()"""
