d76314ff21ea6bce03c132d4ad91afb8e50c2d0b
Ahmed Nassar <Ahmednassar@gmail.com>
2019-03-17 14:30:41 +0200

    test ignorefile

--- faceDetect.py
+++ faceDetect.py
@@ -1,26 +1,133 @@
 import matplotlib.pyplot as plt
 import cv2
+import glob
+import numpy as np
+import tensorflow as tf
+from model import create_model
+from keras import backend as K
+from keras.models import Model
+from itertools import chain
+import dlib
+from align import AlignDlib
+
 
 class Face_detector:
     haar_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")   # we will load cascade classifier into this variable later
     faces = None # we will store detected faces Coordinates
+    hog =   dlib.get_frontal_face_detector()
+    alignment = AlignDlib('shape_predictor_68_face_landmarks.dat')
 
     def __init__(self):
         self.image_path = ""
         self.image = None
-
+        self.grayImage = None
+    
     def loadImage(self):
-        self.image = cv2.imread(self.image_path,0) # load image 
-        #self.image = cv2.cvtColor(self.image,cv2.COLOR_BGR2GRAY) # convert it to gray as cv2 face detector expecte
+        self.image = cv2.imread(self.image_path) # load image 
+        self.grayImage = cv2.cvtColor(self.image,cv2.COLOR_BGR2GRAY) # convert it to gray as cv2 face detector expecte
 
 
-    def isFace(self,imagePath):
-        self.image_path = imagePath
-        self.loadImage()
-        faces = Face_detector.haar_cascade.detectMultiScale(self.image,scaleFactor=1.1,minNeighbors=5)
+    def isFace(self,img,path):
+        self.image_path=path
+        self.grayImage=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        faces = Face_detector.haar_cascade.detectMultiScale(self.grayImage,scaleFactor=1.1,minNeighbors=5)
         if(len(faces)==1):
+            x , y , w , h = faces[0]
+            #allign = Face_detector.alignment.align(96, img,  dlib.rectangle(left=x, top=y, right=x+w, bottom=y+h), landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
+            cv2.imwrite(self.image_path,img[y:y+h,x:x+w])
             return "Accepted Image"
         elif(len(faces)>1):
             return "many faces!"
         elif (len(faces)==0):
             return "no faces detected.."
+
+    def isFace2(self,img,path):
+        self.image_path=path
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        #print(Face_detector.alignment.getAllFaceBoundingBoxes(img))
+        #print(type(Face_detector.alignment.getAllFaceBoundingBoxes(img)))
+        faces = Face_detector.alignment.getAllFaceBoundingBoxes(img)
+        if(len(faces)==1):
+            x , y , w , h = faces[0].left(), faces[0].top() , faces[0].width(), faces[0].height()
+            cv2.imwrite(self.image_path,cv2.resize(img[y:y+h,x:x+w],(96,96)))
+            allign = Face_detector.alignment.align(96, img, faces[0], landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
+            cv2.imwrite(self.image_path+"all.jpg",allign)
+            return "Accepted Image"
+        elif(len(faces)>1):
+            return "many faces!"
+        elif (len(faces)==0):
+            return "no faces detected.."
+    def align(self,img,x,y,w,h):
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        return Face_detector.alignment.align(96, img,  dlib.rectangle(left=x, top=y, right=x+w, bottom=y+h), landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
+class decoder:
+    nn4_small2_pretrained = create_model()
+    nn4_small2_pretrained.load_weights('weights/nn4.small2.v1.h5')
+    def __init__(self):
+        pass
+    
+    def decode_images(self,imagesPath,id,):
+        print("start decoding new user Images...")
+        metaData = 0
+        for i,path in enumerate(imagesPath):
+            img = cv2.imread(path)
+            img = img [...,::-1]
+            img = (img / 255.).astype(np.float32)
+            metaData+=decoder.nn4_small2_pretrained.predict(np.expand_dims(img, axis=0))[0]
+        #metaData/=5
+        print("finish decoding new user Images...returning..\n")
+        return metaData
+
+    def decode_single_image(self,img):
+        img = img [...,::-1]
+        img = (img / 255.).astype(np.float32)
+        img = cv2.resize(img,dsize = (96,96)).reshape(1,96,96,3)
+        return decoder.nn4_small2_pretrained.predict(img)[0]
+    
+    def getIndex(self,current,allUsers):
+        distanc = []#np.array(np.sum(np.square(allUsers - current)),ndmin =1)
+        for i in range(allUsers.shape[0]):
+            d = np.sum(np.square((np.square(allUsers[i]) - np.square(current))))
+            distanc.append(d)
+        print(len(distanc))
+        #list(np.sum(np.square(allUsers - current),axis=1))
+        return min(distanc) , np.argmin(distanc)
+
+    def test(self,testImage,referance):
+        """imgTest = cv2.imread(testImage).resize(96,96).reshape(1,96,96,3)
+        imgTest = decoder.nn4_small2_pretrained.predict(imgTest)[0]
+        re = np.load(referance)
+        """
+        test = decoder.nn4_small2_pretrained.predict(np.expand_dims(testImage, axis=0))[0]
+        re =  decoder.nn4_small2_pretrained.predict(np.expand_dims(referance, axis=0))[0] #referance #
+        l = np.sum(np.square(test - re))#np.linalg.norm(test-re)
+        print(l)
+
+def load_image(path):
+    img = cv2.imread(path, 1)
+    # OpenCV loads images with color channels
+    # in BGR order. So we need to reverse them
+    return img[...,::-1]
+    
+"""nn4_small2_pretrained = create_model()
+nn4_small2_pretrained.load_weights('weights/nn4.small2.v1.h5')
+haar_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")   # we will load cascade classifier into this variable later
+
+img1 = load_image("trainingData\\Gerhard_Schroeder\\Gerhard_Schroeder_0007.jpg")
+gray = cv2.cvtColor(img1,cv2.COLOR_RGB2GRAY)
+f = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5)
+x ,y,w,h=f[0]
+img1 = cv2.resize(img1[y:y+h,x:x+h],(96,96))
+img1 = (img1 / 255.).astype(np.float32)
+emb1 = nn4_small2_pretrained.predict(np.expand_dims(img1, axis=0))[0]
+
+img2 = load_image("trainingData\\Gerhard_Schroeder\\Gerhard_Schroeder_0005.jpg")
+gray = cv2.cvtColor(img2,cv2.COLOR_RGB2GRAY)
+f = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5)
+x ,y,w,h=f[0]
+img2 = cv2.resize(img2[y:y+h,x:x+h],(96,96))
+img2 = (img2 / 255.).astype(np.float32)
+emb2 = nn4_small2_pretrained.predict(np.expand_dims(img2, axis=0))[0]
+
+print(np.sum(np.square(emb1 - emb2)))
+"""
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from faceDetect import Face_detector as fc
+from faceDetect import decoder 
 import shutil
 
 
@@ -16,6 +17,8 @@ def creatData():
 
     if not os.path.exists("attendance-data"):
         os.makedirs("attendance-data")
+    if not os.path.exists("attendance-data\metaData"):
+        os.makedirs("attendance-data\metaData")
     data = {"ID":[],"Name":[],"Path":[]}
     if not os.path.isfile("attendance-data\\data.csv"):
         df = pd.DataFrame(data = data,columns=['ID','Name','Path'])
@@ -59,10 +62,22 @@ def takePic(name):
     print("Please take 5 photos!, press Enter to take photo or 0 to quit")
     cam = cv2.VideoCapture(0)
     imageCounter = 0
+    imagesNames=[]
+    #path2 = input("Enter Image Path")
+    #frame = cv2.imread(path2)
+    imgName = "{}-pic-{}.png".format(name,0)
+    #isFace = faceCheck.isFace(frame,os.path.join(path,imgName))
+    imagesNames.append(os.path.join(path,imgName))
+    q = False
     while(True):
         # initalize cv2 to take pic
         ret,frame = cam.read()
+        grayImage = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = faceCheck.haar_cascade.detectMultiScale(grayImage,scaleFactor=1.1,minNeighbors=5)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)
         cv2.imshow("Take Photos",frame)
+        
         #make sure that cam work
         if not ret :
             print("error in take photos!")
@@ -75,9 +90,10 @@ def takePic(name):
             break
         # wait for user to press ENTER or 0 or d key 
         elif key%256 == 13:
-            imgName = "{}-pic-{}.png".format(name,imageCounter+1) # generate image name using it's number 
-            cv2.imwrite(os.path.join(path,imgName),frame) # save image in user folder 
-            isFace = faceCheck.isFace(os.path.join(path,imgName)) # check if there are faces in pic (object return [accepted image or many faces or no faces detcted])
+            imgName = "{}-pic-{}.png".format(name,imageCounter) # generate image name using it's number 
+            imagesNames.append(os.path.join(path,imgName))
+            #cv2.imwrite(os.path.join(path,imgName),frame) # save image in user folder 
+            isFace = faceCheck.isFace(frame,os.path.join(path,imgName)) # check if there are faces in pic (object return [accepted image or many faces or no faces detcted])
             if(isFace=="Accepted Image"): # accept pic if there are only one face in it 
                 imageCounter+=1
                 print("image {} saved".format(imgName)) # save accepted image 
@@ -85,16 +101,18 @@ def takePic(name):
                 print(isFace,"please try again!, or enter 0 to quit")
 
         elif key%256 == 48: # user press 0 
+            q = True
             print("WARNING: all new user data will REMOVE!, press d to quit.")
         
-        elif key == ord('d'): # delete pic and exit 
+        elif key == ord('d') and q: # delete pic and exit 
             print("delete data and exit ...")
             shutil.rmtree(path)
             exit(0)
     # end cam process   
     cam.release() 
     cv2.destroyAllWindows()
-    return id,path # return generated id and image folder path
+    
+    return id,path,imagesNames# return generated id and image folder path
 
 def showAllData():
     """
@@ -110,8 +128,8 @@ def showAllData():
     print(df)
     print("\n..................................................\n")
 
-
 def showUserPic(id):
+
     """
     this function plot 5 pic of user with the givin id 
     args:
@@ -148,19 +166,83 @@ def addNewUser():
     """
     this function handle the hole process of adding new user int our data, calling helper function like (takePic,appendData)
     """
+    first = True
+    if os.path.exists("attendance-data\\metaData\\allMeta.npy"):
+        first = False
+        allMetaData = np.load("attendance-data\\metaData\\allMeta.npy")
+    else:
+        allMetaData = np.zeros((1,128))
     name = str(input("Enter new user name : ")) # get new user name 
-    id,path=takePic(name)
+    id,path,imagesNames=takePic(name)
     appendData(id,name,path)
-    print("new user added !\nID:{}, name:{}".format(id,name))
+
+    print(imagesNames[0])
+    test = cv2.imread(imagesNames[0],1)
+    print(test)
+    newMeta = np.array(features.decode_single_image(test)).reshape((1,128))
+    print("meta shape is : ",newMeta.shape)
+    if not first:
+        print("new meta: {}, all meta : {}".format(newMeta.shape,allMetaData.shape))
+        allMetaData =  np.vstack([allMetaData,newMeta])
+    else:
+        allMetaData = newMeta
+    #allMetaData.append(newMeta)
+    del newMeta
+    np.save("attendance-data\\metaData\\allMeta",np.array(allMetaData))
+    del allMetaData
+    print("new user added !\nID: {}, name:{}".format(id,name))
     print("\n..................................................\n")
+
+def predict():
+    currentMetaData = np.load("attendance-data\\metaData\\allMeta.npy")
+    currentUserInfo = pd.read_csv("attendance-data\\data.csv")
+    names = currentUserInfo['Name']
+    cam = cv2.VideoCapture(0)
+    cam.get(cv2.CAP_PROP_FPS)
+    while(True):
+        # initalize cv2 to take pic
+        ret,frame = cam.read()
+        grayImage = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = faceCheck.haar_cascade.detectMultiScale(grayImage,scaleFactor=1.1,minNeighbors=5)
+        if(len(faces)!=0):
+            for (x,y,w,h) in faces:
+                dist = 10
+                name = ""
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)
+                currentFrame = np.array(features.decode_single_image(frame[y:y+h,x:x+w]).reshape((1,128)))
+                dist , ind = features.getIndex(currentFrame,currentMetaData)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                if(dist<0.5):
+                    name = names[ind]
+                else:
+                    name = "Nan!!"
+                print(dist)
+                cv2.putText(frame, name, (x, y + h + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2, cv2.LINE_AA)
+        #make sure that cam work
+        cv2.imshow("Take Photos",frame)
+        if not ret :
+            print("error in take photos!")
+            break
+        # init wait key
+        key = cv2.waitKey(1) & 0xFF
+               
+        if key == ord('q') : # delete pic and exit 
+            print("exit ...")
+            break
+    # end cam process   
+    cam.release() 
+    cv2.destroyAllWindows()
 
 
 def main():
     creatData()
     while(True):
+        task=""
         task = (input("enter num of what you want to do?\n1)Add new use ?,  2)show All data ? , 3)plot user pic? , 4)quit : "))
         if(task=='1'):
             addNewUser()
+            a = np.load("attendance-data\\metaData\\allMeta.npy")
+            print(a.shape)
         elif(task=='2'):
             showAllData()
         elif(task=='3'):
@@ -169,10 +251,50 @@ def main():
         elif (task=='4'):
             print("exit..")
             exit(0)
+        elif(task=='5'):
+            """test = "attendance-data\\2 - ahmed2\\ahmed2-pic-2.png"
+            re = "attendance-data\\metaData\\2.npy"
+            features.test(test,re)"""
+            predict()
+
         else:
             task = (input("wrong num! please enter num of option you want to do \n1)Add new use ?,  2)show All data ? , 3)plot user pic? , 4)quit : "))
-
+        
 
 if __name__== "__main__":
     faceCheck = fc() # declare global object of face detector class  
+    features = decoder()
     main()
+    #img = cv2.imread("trainingData\\Gerhard_Schroeder\\Gerhard_Schroeder_0007.jpg")
+    """
+    img = cv2.imread("attendance-data\\2 - ayman\\ayman-pic-4.png")
+    grayImage = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = faceCheck.haar_cascade.detectMultiScale(grayImage,scaleFactor=1.1,minNeighbors=5)
+    x,y,w,h = faces[0]
+    img = cv2.resize( img[y:y+h,x:x+w],(96,96))
+    cv2.imshow('image1',img)
+    img= img[...,::-1]/255
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    
+    img2 = cv2.imread("E:\\me\\my pic\\DSCN3534.jpg")
+    grayImage = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    faces = faceCheck.haar_cascade.detectMultiScale(grayImage,scaleFactor=1.1,minNeighbors=5)
+    x,y,w,h = faces[0]
+    img2 = cv2.resize( img2[y:y+h,x:x+w],(96,96))
+    cv2.imshow('image2',img2)
+    img2 = img2[...,::-1]/255
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    """
+    #img2 = np.load("attendance-data\\metaData\\allMeta.npy")
+
+    #img = cv2.imread("attendance-data\\trainingData\Ariel_Sharon\\Ariel_Sharon_0001.jpg")
+    #img2 = cv2.imread("attendance-data\\trainingData\Ariel_Sharon\\Ariel_Sharon_0003.jpg")
+    #img2 = np.load("attendance-data\\metaData\\allMeta.npy")
+    features.test(img,img2)
+    """
+a = np.load("attendance-data\\metaData\\allMeta.npy")
+print(a.shape)
+"""
